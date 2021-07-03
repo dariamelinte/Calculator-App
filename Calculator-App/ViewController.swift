f01e7cbf0be14eae6f4c7ff5723c9dd8f4a0ded6
daria melinte <dariamelinte2003@gmail.com>
2021-07-03 10:20:34 +0300

    change the views to buttons and make the number buttons concat into a sum

--- Calculator-App/ViewController.swift
+++ Calculator-App/ViewController.swift
@@ -7,13 +7,74 @@
 
 import UIKit
 
+enum Signs {
+    case ac, changeSign, procent, divide, multiply, decrease, increase, point, equal;
+}
+
+var Numbers: Array = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
 class ViewController: UIViewController {
 
+    var sum = "0";
+    
+    var currentSign = "";
+    
+    func changeSignOfTheCalculus (sign: Signs) {
+//        switch sign {
+//        case sign === "-:
+//            <#code#>
+//        default:
+//            <#code#>
+//        }
+    }
+    
+    func addNumberToSum (number: Int) {
+        sum = sum + String(number);
+        resultsLabel.text = sum;
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        resultsLabel.text = sum;
+    }
+    
+    @IBOutlet weak var resultsLabel: UILabel!
+    
+    @IBAction func numberButton (_ button: UIButton) {
+        if (sum == "0") {
+            sum = button.currentTitle!;
+        } else {
+            sum = sum + button.currentTitle!;
+        }
+        resultsLabel.text = sum;
+    }
+    
+    @IBAction func acButton(_ sender: Any) {
+    }
+    
+    @IBAction func changeSignButton(_ sender: Any) {
     }
 
-
+    @IBAction func procentButton(_ sender: Any) {
+    }
+    
+    @IBAction func divideButton(_ sender: Any) {
+    }
+    
+    @IBAction func multiplyButton(_ sender: Any) {
+    }
+    
+    @IBAction func decreaseButton(_ sender: Any) {
+    }
+    
+    @IBAction func increaseButton(_ sender: Any) {
+    }
+    
+    @IBAction func pointButton(_ sender: Any) {
+    }
+    
+    @IBAction func equalButton(_ sender: Any) {
+    }
 }
 
